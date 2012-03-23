a8e5a1a2c086d4a662a4c22db1cae93e83abd549
Mantas Mikulėnas <grawity@nullroute.eu.org>
2012-03-23 21:23:37 +0200

    lib-php: fix interval($end)

--- lib-php/librwho.php
+++ lib-php/librwho.php
@@ -224,8 +224,10 @@ function normalize_host($host) {
 // time interval: "X days", "Xh Ym", "Xm Ys", "X secs"
 
 function interval($start, $end = null) {
-	if ($end === null)
-		$end = time();
+	if ($end === null) {
+		$end = $start;
+		$start = time();
+	}
 	$diff = $end - $start;
 	$diff -= $s = $diff % 60; $diff /= 60;
 	$diff -= $m = $diff % 60; $diff /= 60;
