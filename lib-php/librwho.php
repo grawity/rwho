08db0e52754ae03b0adc67b9adc4df95250c66f1
Mantas Mikulėnas <grawity@gmail.com>
2012-09-22 18:20:32 +0300

    lib-php: move functions from ui-finger

--- lib-php/librwho.php
+++ lib-php/librwho.php
@@ -421,4 +421,52 @@ function find_user_plan($user, $host) {
 	return null;
 }
 
+// ip_expand(str $addr) -> str?
+// Expand a string IP address to binary representation.
+
+function ip_expand($addr) {
+	$addr = @inet_pton($addr);
+	if ($addr === false || $addr === -1)
+		return null;
+	if (strlen($addr) == 16) {
+		if (substr($addr, 0, 12) === "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xFF\xFF"
+		||  substr($addr, 0, 12) === "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+			return substr($addr, 12);
+	}
+	return $addr;
+}
+
+// ip_cidr(str $host, str $cidrmask) -> bool
+// Check if $host belongs to the network $mask (specified in CIDR format)
+// If /prefixlen is not specified, an exact match will be done.
+
+function ip_cidr($host, $mask) {
+	@list($net, $len) = explode("/", $mask, 2);
+
+	$host = ip_expand($host);
+	$net = ip_expand($net);
+
+	if ($host === null || $net === null || strlen($host) !== strlen($net))
+		return false;
+
+	$nbits = strlen($net) * 8;
+
+	if ($len === null)
+		$len = $nbits;
+	elseif ($len < 0 || $len > $nbits)
+		return false;
+
+	$host = unpack('C*', $host);
+	$net = unpack('C*', $net);
+
+	for ($i = 1; $i <= count($net) && $len > 0; $i++) {
+		$bits = $len >= 8 ? 8 : $len;
+		$bmask = (0xFF << 8 >> $bits) & 0xFF;
+		if (($host[$i] & $bmask) != ($net[$i] & $bmask))
+			return false;
+		$len -= 8;
+	}
+	return true;
+}
+
 return true;
