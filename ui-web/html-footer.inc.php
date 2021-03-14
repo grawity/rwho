3deca21ce94904c4f7adf93f861159859a33bb13
Mantas Mikulėnas <grawity@gmail.com>
2021-03-14 16:18:05 +0200

    ui-web: use json_encode() to format the JS object

--- ui-web/html-footer.inc.php
+++ ui-web/html-footer.inc.php
@@ -1,10 +1,10 @@
 <?php namespace RWho; ?>
 <?php if (html::$refresh) { ?>
 <script type="text/javascript">
-var settings = {
-	interval: <?= html::$refresh ?>,
-	args: "<?= addslashes(Web\mangle_query(["fmt" => "html-xhr"])) ?>"
-};
+var settings = <?= json_encode([
+	"interval" => html::$refresh,
+	"args" => Web\mangle_query(["fmt" => "html-xhr"]),
+]) ?>;
 </script>
 <script type="text/javascript" src="xhr.js"></script>
 <?php } ?>
