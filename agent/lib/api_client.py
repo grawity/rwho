73ff50d34a7a3e3d9b52f7ace8b457dccc8e586b
Mantas Mikulėnas <grawity@gmail.com>
2022-01-04 22:09:43 +0200

    agent: port client to JSON-RPC instead of legacy interface

--- agent/lib/api_client.py
+++ agent/lib/api_client.py
@@ -4,45 +4,39 @@ import socket
 import sys
 
 from .exceptions import *
+from .json_rpc import JsonRpcClient, RemoteFault
 from .log_util import *
 
 class RwhoClient():
     def __init__(self, url, host_name=None):
-        self.url = url
+        self.rpc = JsonRpcClient(url)
         self.host_name = host_name
-        self.ua = requests.Session()
 
     def auth_set_basic(self, username, password):
         import requests.auth
-        self.ua.auth = requests.auth.HTTPBasicAuth(username, password)
+        self.rpc.ua.auth = requests.auth.HTTPBasicAuth(username, password)
 
     def auth_set_kerberos(self, service="HTTP"):
         import gssapi
         import requests_gssapi
         spnego = gssapi.Mechanism.from_sasl_name("SPNEGO")
-        self.ua.auth = requests_gssapi.HTTPSPNEGOAuth(target_name=service,
-                                                      mech=spnego,
-                                                      opportunistic_auth=True)
+        self.rpc.ua.auth = requests_gssapi.HTTPSPNEGOAuth(target_name=service,
+                                                          mech=spnego,
+                                                          opportunistic_auth=True)
 
-    def _upload(self, action, data):
-        log_debug("api: calling %r with %d items", action, len(data))
-        payload = {
-            "host": self.host_name,
-            "action": action,
-            "utmp": json.dumps(data),
-        }
-        resp = self.ua.post(self.url, data=payload)
-        resp.raise_for_status()
-        log_debug("api: server returned %r", resp.text)
-        if resp.text.strip() == "OK":
-            return True
-        elif resp.text.startswith("KOD"):
-            raise RwhoShutdownRequestedError(resp.text.strip())
-        else:
-            raise RwhoUploadRejectedError(resp.text.strip())
+    def call(self, method, *args):
+        try:
+            return self.rpc.call(method, args)
+        except RemoteFault as e:
+            if e.code == 403:
+                raise RwhoUploadRejectedError(e.message) from None
+            elif e.code == 410:
+                raise RwhoShutdownRequestedError(e.message) from None
+            else:
+                raise
 
     def put_sessions(self, sessions):
-        return self._upload(action="put", data=[*sessions])
+        return self.call("PutEntries", self.host_name, [*sessions])
 
     def remove_host(self):
-        return self._upload(action="destroy", data=[])
+        return self.call("ClearEntries", self.host_name)
