d31e6e9c3abd9684e65d20c069f41e25922b13c4
Mantas Mikulėnas <grawity@gmail.com>
2022-01-08 17:23:56 +0200

    init.sql: widen (and align) columns

--- init.sql
+++ init.sql
@@ -2,18 +2,18 @@ START TRANSACTION;
 
 DROP TABLE IF EXISTS utmp;
 CREATE TABLE utmp (
-	rowid	integer		AUTO_INCREMENT,
-	host	varchar(255)	NOT NULL,
+	rowid		integer		AUTO_INCREMENT,
+	host		varchar(255)	NOT NULL,
 	-- normally UT_NAMESIZE, but allow more for Windows
-	user	varchar(64)	NOT NULL,
-	rawuser	varchar(64)	NOT NULL,
-	uid	integer,
+	user		varchar(64)	NOT NULL,
+	rawuser		varchar(64)	NOT NULL,
+	uid		integer,
 	-- UT_HOSTSIZE
-	rhost	varchar(256),
+	rhost		varchar(256),
 	-- UT_LINESIZE
-	line	varchar(32),
-	time	integer,
-	updated	integer,
+	line		varchar(32),
+	time		integer,
+	updated		integer,
 	-- indexes
 	PRIMARY KEY (rowid)
 );
